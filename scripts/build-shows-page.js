568ceea8937bfe68dab6d8fc881f505b802fdd73
Aranyah <20851992+aranyah@users.noreply.github.com>
2025-01-12 13:25:14 -0500

    Organizing JS for shows page with better naming and structure

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,63 +1,90 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const heroPlayer = document.querySelector('.hero__player');
+const API_KEY = "8fd3018e-f075-45f1-b31a-024ab1909a89";   
+const bandSiteApi = new BandSiteApi(API_KEY);
 
-  const soundcloudEmbed = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/252516215&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 0.625rem; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/queen-69312" title="Queen" target="_blank" style="color: #cccccc; text-decoration: none;">Queen</a> · <a href="https://soundcloud.com/queen-69312/dont-stop-me-now-remastered" title="Don&#x27;t Stop Me Now (Remastered 2011)" target="_blank" style="color: #cccccc; text-decoration: none;">Don&#x27;t Stop Me Now (Remastered 2011)</a></div>`;
-  heroPlayer.innerHTML = soundcloudEmbed;
 
-  const showsData = [
-      {
-          date: 'Mon Sept 09 2024',
-          venue: 'Ronald Lane',
-          location: 'San Francisco, CA'
-      },
-      {
-          date: 'Tue Sept 17 2024',
-          venue: 'Pier 3 East',
-          location: 'San Francisco, CA'
-      },
-      {
-          date: 'Sat Oct 12 2024',
-          venue: 'View Lounge',
-          location: 'San Francisco, CA'
-      },
-      {
-          date: 'Sat Nov 16 2024',
-          venue: 'Hyatt Agency',
-          location: 'San Francisco, CA'
-      },
-      {
-          date: 'Fri Nov 29 2024',
-          venue: 'Moscow Center',
-          location: 'San Francisco, CA'
-      },
-      {
-          date: 'Wed Dec 18 2024',
-          venue: 'Press Club',
-          location: 'San Francisco, CA'
-      }
-  ];
+
+const showlistElement = document.querySelector(".shows__list");
+// const main = document.querySelector('.main');
+
+
+
+
+
+const showsSection = document.createElement('section');
+showsSection.className = 'shows';
+
+const showsHeader = document.createElement('div');
+showsHeader.classList.add('shows__table-header');
+
+// Use this as reference from index-js
+// const listElement = document.createElement("li");
+// listElement.classList.add("conversation__entry");
+
+
+const showsList = document.createElement('div');
+showsList.className = 'shows__list';
+
+const headerLabels = document.createElement('div');
+headerLabels.className = 'shows__item shows__item--header';
+['DATE', 'VENUE', 'LOCATION'].forEach(label => {
+    
+    
+    const labelDiv = document.createElement('div');
+    labelDiv.className = 'shows__label-primary';
+    labelDiv.textContent = label;
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const heroPlayer = document.querySelector('.hero__player');
+
+//   const soundcloudEmbed = `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/252516215&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 0.625rem; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/queen-69312" title="Queen" target="_blank" style="color: #cccccc; text-decoration: none;">Queen</a> · <a href="https://soundcloud.com/queen-69312/dont-stop-me-now-remastered" title="Don&#x27;t Stop Me Now (Remastered 2011)" target="_blank" style="color: #cccccc; text-decoration: none;">Don&#x27;t Stop Me Now (Remastered 2011)</a></div>`;
+//   heroPlayer.innerHTML = soundcloudEmbed;
+
+
+
+
+
+
+
+
+
+
+
+//   const showsData = [
+//       {
+//           date: 'Mon Sept 09 2024',
+//           venue: 'Ronald Lane',
+//           location: 'San Francisco, CA'
+//       },
+//       {
+//           date: 'Tue Sept 17 2024',
+//           venue: 'Pier 3 East',
+//           location: 'San Francisco, CA'
+//       },
+//       {
+//           date: 'Sat Oct 12 2024',
+//           venue: 'View Lounge',
+//           location: 'San Francisco, CA'
+//       },
+//       {
+//           date: 'Sat Nov 16 2024',
+//           venue: 'Hyatt Agency',
+//           location: 'San Francisco, CA'
+//       },
+//       {
+//           date: 'Fri Nov 29 2024',
+//           venue: 'Moscow Center',
+//           location: 'San Francisco, CA'
+//       },
+//       {
+//           date: 'Wed Dec 18 2024',
+//           venue: 'Press Club',
+//           location: 'San Francisco, CA'
+//       }
+//   ];
 
   const createShowsSection = () => {
-      const main = document.querySelector('.main');
-      const showsSection = document.createElement('section');
-      showsSection.className = 'shows';
-
-      const header = document.createElement('div');
-      header.className = 'shows__header';
-      const title = document.createElement('h2');
-      title.className = 'shows__title';
-      title.textContent = 'Shows';
-      header.appendChild(title);
-
-      const showsList = document.createElement('div');
-      showsList.className = 'shows__list';
-
-      const headerLabels = document.createElement('div');
-      headerLabels.className = 'shows__item shows__item--header';
-      ['DATE', 'VENUE', 'LOCATION'].forEach(label => {
-          const labelDiv = document.createElement('div');
-          labelDiv.className = 'shows__label-primary';
-          labelDiv.textContent = label;
+     
           headerLabels.appendChild(labelDiv);
       });
       showsList.appendChild(headerLabels);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,21 +1,4 @@
 
-// const conversation = [
-//     {
-//       name: "Victor Pinto",
-//       text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-//       date: "11/02/2023",
-//     },
-//     {
-//       name: "Christina Cabrera",
-//       text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-//       date: "10/28/2023",
-//     },
-//     {
-//       name: "Isaac Tadesse",
-//       text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-//       date: "10/20/2023",
-//     },
-//   ];
 const API_KEY = "8fd3018e-f075-45f1-b31a-024ab1909a89";   
 const bandSiteApi = new BandSiteApi(API_KEY);
 
